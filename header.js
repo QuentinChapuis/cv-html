feff138ee34df441fc97f6dc0b4ce594b4176279
tlarpin <thomas.larpin42@gmail.com>
2024-12-24 08:27:09 +0100

    push

--- header.js
+++
@@ -1,6 +0,0 @@
-function affiche(li, div){
-    $('li').attr('class', '')
-    $(li).attr('class', 'active')
-    $('.element').attr('style', 'display:none;')
-    $('#'+div).attr('style', 'display:block;')
-}
